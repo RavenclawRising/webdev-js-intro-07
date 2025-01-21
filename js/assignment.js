a25aaf5f30c9fc72facc8d3146ed93a8f01015c8
unknown <Lauradu@me.com>
2025-01-20 21:00:17 -0500

    Modified README, assignment.js, and index.js

--- js/assignment.js
+++ js/assignment.js
@@ -6,3 +6,37 @@ const checkIfEvenElement = document.getElementById("check-if-even");
 const lostNumbersElement = document.getElementById("lost-numbers");
 
 const lostNumbers = [4, 8, 15, 16, 23, 42];
+
+//Return the reaminder
+const returnTheRemainder = (a, b) => a % b;
+
+
+console.log(returnTheRemainder(4, 5));    
+
+//Check if even
+const checkIfEven = () => {
+    const num = 47;
+    if (num % 2 === 0) {
+        checkIfEvenElement.textContent = true;
+    } else {
+        checkIfEvenElement.textContent = false;
+    }
+};
+
+//The Fourth Element
+const getTheFourthElement = arr => arr[3];
+
+console.log(getTheFourthElement(lostNumbers));    // Output: 16
+
+// Event listener for the submission button
+submissionBtn.addEventListener("click", () => {
+    // Display the remainder
+   returnTheRemainder();
+    
+   // Check if the number is even
+    checkIfEven();
+
+    // Display the fourth element of the lostNumbers array
+    const fourthElement = getTheFourthElement(lostNumbers);
+    lostNumbersElement.textContent = fourthElement;   
+});
--- js/index.js
+++ js/index.js
@@ -2,3 +2,38 @@
 // that are not declared
 "use strict"
 
+/*
+
+
+//A greeting
+function greet (name = "Jamie") {
+    console.log("Hello,  " + name + "!");
+}
+
+greet();            //Output:    Hello, Jamie!
+greet("Kassi");     //Output:    Hello, Kassi!
+
+//Return functions
+function add(a, b) {
+    return a + b;
+}
+
+add(245, 21);       //Output       266
+add(5891, 22212);   //Output       28103
+
+
+/Arrow functions
+const add = (a, b) => a +b ;
+
+add(12, 49);        //Output        61
+add((52), (36));    //Output        88
+
+    
+    
+    
+const logMessage = (message) => {       //Output undefined
+    console.log(message);
+};
+
+
+*/
